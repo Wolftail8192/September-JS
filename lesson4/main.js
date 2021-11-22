1c707a78d89ff290dc69c4a907a399457b554cbb
Wolftail8192 <4420299@gmail.com>
2021-11-22 12:51:42 +0200

    hw4+cw4

--- lesson4/main.js
+++ lesson4/main.js
@@ -111,14 +111,83 @@ function printMaxNumber(a,b,c) {
 
 
 // - створити функцію яка повертає найбільше число з масиву
+
+    function maxFromArray(arr) {
+       let max = numbers[0]
+        for (const number of numbers){
+            if (number>max){
+                max = number;
+            }
+        }
+        return max;
+    }
+
 // - створити функцію яка повертає найменьше число з масиву
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+
+    function minFromArray(arr) {
+        let min = numbers[0]
+        for (const number of numbers){
+            if (number>min){
+                min = number;
+            }
+        }
+        return min;
+    }
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад [1,2,10]->13
+    function sumArr(nums) {
+        let result = 0;
+        for (const num of nums){
+            result=result+num;
+        }
+        return result;
+    }
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+    function avgArr(nums) {
+        return sumArr(nums)/nums.length;
+    }
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+// (Math використовувати заборонено);
+    function maxinArray(nums) {
+        let max=nums[0];
+        let min=nums[0];
+        for (const num of nums){
+            if (num > max){
+                max=num;
+            }
+            if (num<min){
+                min=num;
+            }
+        }
+        console.log(max);
+        return min;
+    }
 // - створити функцію яка заповнює масив рандомними числами
-// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100))
+// та виводить його.
+    function ramdomazer(arr) {
+        for (let i=0;i<100;i++){
+            arr[i]=Math.round(Math.random()*100);
+        }
+        return arr;
+    }
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit.
+// limit - аргумент, який характеризує кінцеве значення діапазону.
+    function ramdomazer2(arr,limit) {
+        for (let i=0;i<arr.length;i++){
+            arr[i]=Math.round(Math.random()*limit);
+        }
+        return arr;
+    }
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+    function reverse(arr) {
+        let newArr=[];
+        for(let i=newArr.length-1, ri=0; i>=0; i--, ri++){
+            newArr[ri]=newArr[i]
+        }
+        return newArr;
+    }
 //
 //
 //     additional4
