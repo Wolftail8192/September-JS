3cfa12f8560783bb8cf4ac8bebdfb08b670b2720
Wolftail8192 <4420299@gmail.com>
2021-11-23 20:58:27 +0200

    hw5+cw5

--- lesson5/main.js
+++ lesson5/main.js
@@ -61,16 +61,125 @@ let arrayOfObjects=(...arr)=> {
 // classwork
 // Всі функції повинні бути описані стрілочним типом!!!!
 // - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+let printMinNumber=(a,b,c)=> {
+        if (a<b && a<c){
+            if(b<c){
+                console.log(a)
+            }else{
+                console.log(a)
+            }
+        }else if(b<a && b<c){
+            if(a<c){
+                console.log(b)
+            }else{
+                console.log(b)
+
+            }
+        }else if (c<a && c<b){
+            if (a<b){
+                console.log(c)
+            }else{
+                console.log(c)
+            }
+        }
+    }
+    printMinNumber(12,13,14)
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+let printMaxNumber=(a,b,c) =>{
+    if (a>b && a>c){
+        if(b>c){
+            console.log(a)
+        }else{
+            console.log(a)
+        }
+    }else if(b>a && b>c){
+        if(a>c){
+            console.log(b)
+        }else{
+            console.log(b)
+
+        }
+    }else if (c>a && c>b){
+        if (a>b){
+            console.log(c)
+        }else{
+            console.log(c)
+        }
+    }
+}
+printMaxNumber(12,13,14)
 // - створити функцію яка повертає найбільше число з масиву
+let maxFromArray=(arr)=> {
+       let max = numbers[0]
+        for (const number of numbers){
+            if (number>max){
+                max = number;
+            }
+        }
+        return max;
+    }
 // - створити функцію яка повертає найменьше число з масиву
+let minFromArray=(arr) =>{
+        let min = numbers[0]
+        for (const number of numbers){
+            if (number>min){
+                min = number;
+            }
+        }
+        return min;
+    }
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+let sumArr=(nums)=> {
+        let result = 0;
+        for (const num of nums){
+            result=result+num;
+        }
+        return result;
+    }
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+let avgArr=(nums)=> {
+        return sumArr(nums)/nums.length;
+    }
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+// (Math використовувати заборонено);
+let maxinArray=(nums)=> {
+        let max=nums[0];
+        let min=nums[0];
+        for (const num of nums){
+            if (num > max){
+                max=num;
+            }
+            if (num<min){
+                min=num;
+            }
+        }
+        console.log(max);
+        return min;
+    }
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+let ramdomazer=(arr)=> {
+        for (let i=0;i<100;i++){
+            arr[i]=Math.round(Math.random()*100);
+        }
+        return arr;
+    }
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент,
+// який характеризує кінцеве значення діапазону.
+let ramdomazer2=(arr,limit)=> {
+        for (let i=0;i<arr.length;i++){
+            arr[i]=Math.round(Math.random()*limit);
+        }
+        return arr;
+    }
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+let reverse=(arr)=> {
+        let newArr=[];
+        for(let i=newArr.length-1, ri=0; i>=0; i--, ri++){
+            newArr[ri]=newArr[i]
+        }
+        return newArr;
+    }
 //
 //
 // additional
